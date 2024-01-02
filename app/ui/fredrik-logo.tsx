5754370c2ebec6555d11a292ee2ffc2d3c2df046
Fredrir <fhansteen@gmail.com>
2024-01-02 20:07:46 +0100

    Initial start of portofolio

---
+++ app/ui/fredrik-logo.tsx
@@ -0,0 +1,13 @@
+import { CodeBracketIcon} from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function FredrikLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <CodeBracketIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Fredrik Hansteen</p>
+    </div>
+  );
+}
